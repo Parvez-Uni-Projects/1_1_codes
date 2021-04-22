0c138f234177543df57f89fd93875c112d10b456
Parvez Ahammed <parvezdirom2000@gmail.com>
2021-04-23 00:32:36 +0600

    hululu

--- Toph/07_string_palindrome.c
+++ Toph/07_string_palindrome.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main ()
 {
-    char str1[20],str2[20];
+    char str1[20],str2[20];w
     int flag;
     gets(str1);
     for (int i=strlen(str1)-1,j=0 ;i>=0;i--,j++)
@@ -13,11 +13,13 @@ int main ()
     for (int i=0;i<strlen(str1);i++)
         {
             if(str1[i]==str2[i])
+            {
                 flag=1;
+            }  
+               
             else
             {
-                  flag=0;
-                  break ;
+                  flag=0;            break ;
             }
 
         }
