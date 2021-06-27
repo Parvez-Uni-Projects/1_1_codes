ee7f06b2df7f67d520ecbc8b801c7a64db1e42ce
Parvez Ahammed <parvezdirom2000@gmail.com>
2021-06-27 21:28:59 +0600

    cg calcultor project initiated

--- personal project/cgcalcuusinggrade.c
+++ personal project/cgcalcuusinggrade.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+float grade_conv(char);
 int main ()
 {
     char coursegrade[9];
@@ -24,5 +25,30 @@ int main ()
         return 0;
 
 }
+
+float grade_conv(char grade)
+{
+    if (grade == 'A+' || grade =='a+')
+        return 4;
+    else if (grade == 'A' || grade =='a')
+        return 3.75;
+    else if (grade == 'A-' || grade =='a-')
+        return 3.5;
+    else if (grade == 'B+' || grade =='B+')
+        return 3.25;
+    else if (grade == 'B' || grade =='b')
+        return 3;
+    else if (grade == 'B-' || grade =='b-')
+        return 2.75;
+    else if (grade == 'C+' || grade =='c')
+        return 2.5;
+     else if (grade == 'C' || grade =='c')
+        return 2.25;
+    else if (grade == 'D' || grade =='d')
+        return 2;
+    else if (grade == 'F' || grade =='f')
+        return 0;
+}
+
 //! The main logic will be ((3*sum of course no 1-5) + (1.5* course no 6-8) + (.75 * course no 8)) / 20.25 
 //! We will be storing the sum of each types of courses in different types of arrays  
